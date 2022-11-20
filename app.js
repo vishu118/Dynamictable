752ed904cb0f202838872cbfab7abca4fe170460
vishu118 <114770536+vishu118@users.noreply.github.com>
2022-11-20 21:59:58 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -54,50 +54,7 @@ formEl.addEventListener('submit',(e)=>{
 })
 
 
-// function createTable(e){
-//     e.preventDefault();
-//     const nameVal = nameEl.value;
-//     const emailVal = emailEl.value;
-//     const phoneVal = phoneEl.value;
-//     if (nameVal == "" || emailVal == "" || phoneVal == "") {
-//         errorEl.innerText = "Empty input field (s)";
-//         errorEl.classList.add("error");
-//         setTimeout(() => {
-//             errorEl.innerText = "";
-//         }, 1000);
-//     }
-    
-//     if(nameVal && emailVal && phoneVal){
-//         tbodyEl.innerHTML += `
-//        <tr>
-//        <td class="name-td">${nameVal}</td>
-//        <td class="mail-td">${emailVal}</td>
-//        <td class="num-td">${phoneVal}</td>
-//        <td class="btn-td"><button class="delete-btn">Delete Row</button></td>
-//        </tr>`
-     
-     
-//        nameEl.value = "";
-//        emailEl.value = "";
-//        phoneEl.value = ""; 
-//        //errorEl.style.display = "block"; 
-//     }
-// }
 
-// formEl.addEventListener('submit', createTable);
-
-// tableEl.addEventListener("click",(e)=>{
-//     if(!e.target.classList.contains("delete-btn")){
-//         return;
-//     } 
-//     const btn = e.target;
-//     btn.closest('tr').remove();
-//     successEl.innerText = "Item Deleted Successfully";
-//     successEl.classList.add("success");
-//     setTimeout(() => {
-//         successEl.innerText = "";
-//     }, 1000);
-// })
 
 
 
